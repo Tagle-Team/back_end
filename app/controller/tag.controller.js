908f02f49df8a74afba08ab7ddcb87eb58d6bfa9
jj-pa <krap9482@gmail.com>
2020-12-18 19:06:27 +0900

    Add API for tag

--- app/controller/tag.controller.js
+++ app/controller/tag.controller.js
@@ -3,6 +3,67 @@ const Tag = db.tag;
 const {generateExampleCards, getRandomEmoji} = require('../helpers');
 const shortid = require('shortid');
 
+exports.createTag = (req, res) => {
+  const newTag = new Tag();
+  newTag.title = req.body;
+  newTag.lists = [];
+  newTag.save((err, Tag) => {
+    if (err) {
+      return res.status(400).send({
+        message: 'Failed to add tag. (' + err + ')',
+      });
+    } else {
+      return res.send({
+        message: 'Tag added successfully.',
+      });
+    }
+  });
+}
+
+exports.deleteTag = (req, res) => {
+  const { tagId } = req.body;
+  Tag.destroy({
+    where: { _id: tagId }
+  })
+    .then(result => {
+      return res.send({
+        message: 'Success',
+      });
+    })
+    .catch(err => {
+      return res.send({
+        message: 'Fail',
+      });
+    });
+}
+
+exports.reorderTag = (req, res) => {
+  const {listId, sourceId, sourceIndex, destinationIndex} = req.body;
+
+  Tag.findOneAndUpdate({
+    _id: sourceId
+  }, {
+    $pull: {lists: {_id: listId}}
+  })
+    .then(result => {
+      const list = result.lists[sourceIndex];
+      Tag.updateOne(
+        {_id: sourceId}
+      ), {
+        $push: {
+          lists: {$each: [list], $position: destinationIndex}
+        }
+      }
+      return res.send({ result });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.send({
+        message: 'Fail'
+      });
+    });
+}
+
 exports.example = (req, res) => {
   const exampleCards = generateExampleCards();
   const newTag = new Tag();
--- app/routes/tag.routes.js
+++ app/routes/tag.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { example } = require('../controller/tag.controller');
+const { example, createTag, deleteTag, reorderTag } = require('../controller/tag.controller');
 
 router.post('/example', example);
+router.post('/tag', createTag);
+router.delete('/tag', deleteTag);
+router.put('/tag', reorderTag);
 
 module.exports = router;
